c00c52524952dccc31c63235f268cf585853e6fa
jjaejun <leejj3240@gmail.com>
2023-11-13 11:06:54 +0900

    Dog#bark 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,5 +1,7 @@
 package com.sh.app;
 
+import com.sh.app.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,5 +9,6 @@ public class Main {
 		System.out.println("Hello Git");
 		
 //		new Foo().test();
+		new Dog().bark();
 	}
 }
---
+++ src/com/sh/app/animal/Dog.java
@@ -0,0 +1,8 @@
+package com.sh.app.animal;
+
+public class Dog {
+
+	public void bark() {
+		System.out.println("멍멍~");
+	}
+}
