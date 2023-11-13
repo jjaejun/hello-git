ea796b1d0310e691baadf4d9ba6d1d23e9303c00
jjaejun <leejj3240@gmail.com>
2023-11-13 12:12:16 +0900

    Cat#jump 관련 기능 취소

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,6 +1,5 @@
 package com.sh.app;
 
-import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
 import com.sh.app.animal.Eagle;
 import com.sh.app.animal.Snake;
@@ -15,7 +14,6 @@ public class Main {
 		System.out.println("동물기능 시작~");
 		System.out.println("Animal Go~");
 		new Dog().bark();
-		new Cat().jump();
 		new Eagle().fly();
 		new Snake().crawl();
 		new Tiger().ride();
